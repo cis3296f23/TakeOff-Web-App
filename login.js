60afd05dcb32d1be12005c7fa0d9e123db328a2c
tul58767 <tul58767@temple.edu>
2023-11-29 21:28:07 -0500

    test

--- login.js
+++ login.js
@@ -72,25 +72,4 @@ document
     }
   }
 
-  function openPopup() {
-    var popupContainer = document.getElementById("popupContainer");
-    popupContainer.style.visibility = "visible";
-  }
   
-  function closePopup() {
-    var popupContainer = document.getElementById("popupContainer");
-    popupContainer.style.visibility = "hidden";
-  }
-  
-  function toggleForm() {
-    var loginForm = document.getElementById("loginForm");
-    var signupForm = document.getElementById("signupForm");
-  
-    if (loginForm.style.display === "none") {
-      loginForm.style.display = "block";
-      signupForm.style.display = "none";
-    } else {
-      loginForm.style.display = "none";
-      signupForm.style.display = "block";
-    }
-  }
